c4be2f3f4f6dec97ccd70cc61ca50cec5d67b518
Max Zvy <zvyagin.maksim@inbox.ru>
2023-11-11 17:25:44 +0700

    Запросы

---
+++ Notes.Backend/Notes.Application/Notes/Query/GetNoteDetails/GetNoteDetailsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Query.GetNoteDetails
+{
+    internal class GetNoteDetailsQueryHandler
+        : IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteDetailsQueryHandler(IMapper mapper, INotesDbContext dbContext)
+        {
+            _mapper = mapper;
+            _dbContext = dbContext;
+        }
+
+        public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request,
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes
+                .FirstOrDefaultAsync(note =>
+                 note.Id == request.Id, cancellationToken);
+
+            if(entity == null || entity.UserId != request.UserId) 
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
--- Notes.Backend/Notes.Persistence/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Notes.Persistence
-{
-    public class Class1
-    {
-
-    }
-}
